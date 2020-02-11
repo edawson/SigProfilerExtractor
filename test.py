a9cc43ddba2271ec68ff4d1a6a93399386713325
Eric T Dawson <eric.t.dawson@gmail.com>
2020-02-11 16:30:37 +0000

    Add a basic test for the GPU

--- test.py
+++ test.py
@@ -6,9 +6,19 @@ Created on Wed Jul 10 11:45:38 2019
 @author: mishugeb
 """
 from SigProfilerExtractor import sigpro as sig
+import torch
+
+def test_cpu(data):
+    sig.sigProfilerExtractor("text", "example_output", data, startProcess=1, endProcess=2, totalIterations=3)
+
+def test_gpu(data):
+    if torch.cuda.device_count() > 0:
+        sig.sigProfilerExtractor("text", "example_output", data, startProcess=1, endProcess=2, totalIterations=3, gpu=True)
+
 def main():
     data = sig.importdata("text")
-    sig.sigProfilerExtractor("text", "example_output", data, startProcess=1, endProcess=2, totalIterations=3)
+    test_cpu(data)
+    test_gpu(data)
 
 if __name__ == '__main__':
     main()
